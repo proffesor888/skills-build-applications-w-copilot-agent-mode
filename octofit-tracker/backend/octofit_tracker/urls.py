753914a3ba996114e6c09dfa98add2e90951327a
proffesor888 <proffesor888@gmail.com>
2025-10-18 17:59:13 +0000

    Add CODESPACE_NAME support for dynamic base URL configuration

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import os
+
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    base_url = f"https://{codespace_name}-8000.app.github.dev"
+else:
+    base_url = "http://localhost:8000"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('octofit_tracker.octofit_tracker.backend.octofit_tracker.urls')),
 ]
