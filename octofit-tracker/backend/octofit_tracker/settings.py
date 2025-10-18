d57689489fb047327328aac8351996646455e512
proffesor888 <proffesor888@gmail.com>
2025-10-18 17:55:09 +0000

    Update URL routing and allowed hosts for CODESPACE_NAME support

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-8^g^+l!b(_*&ru0^zj655^0w*!-gpk*4!@#!9sdu368*1cqa-7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
